ef25fa45db81827c77aeda1ceb9304e59d2facb8
黃士桓 <huan5678@gmail.com>
2023-09-05 23:24:38 +0800

    [fix]

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
-import { defineConfig } from "file:///workspace/node_modules/vite/dist/node/index.js";
-import react from "file:///workspace/node_modules/@vitejs/plugin-react-swc/index.mjs";
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
